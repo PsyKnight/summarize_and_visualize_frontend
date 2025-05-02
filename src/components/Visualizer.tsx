7d8e93fd2b5817ab93a192733da76654d6edd52c
Ishanya Kukade <ishanyak12@gmail.com>
2025-05-03 00:45:48 +0530

    Add copy functionality for summary and image

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,27 +1,52 @@
 import { useInputContext } from "../context/InputContext.tsx";
 import Loader from "./Loader.tsx";
+import { FaCopy } from "react-icons/fa";
 
 const Visualizer = () => {
   const { imgUrl, loading } = useInputContext();
+
+  const copyImageToClipboard = async () => {
+    if (!imgUrl) return;
+    try {
+      const response = await fetch(imgUrl);
+      const blob = await response.blob();
+      const clipboardItem = new ClipboardItem({ [blob.type]: blob });
+      await navigator.clipboard.write([clipboardItem]);
+      console.log("Image copied to clipboard");
+    } catch (error) {
+      console.error("Copy failed:", error);
+    }
+  };
+
   return (
     <section className="min-w-[80vw] flex flex-col items-center justify-center ">
-      <h1 className="text-3xl p-2 bg-gray-400/70 max-w-[80%] min-w-[80vw] rounded-lg rounded-b-none text-center">
-        {loading ? (
-          <div className="flex items-center justify-center">
-            <Loader className="w-12" />
-            <p>Generating image...</p>
-          </div>
-        ) : (
-          "Visualize"
-        )}
-      </h1>
-      <div className="p-4 bg-gray-300 max-w-[80%] min-w-[80vw] rounded-lg rounded-t-none">
+      <div className="flex justify-between p-2 bg-gray-400/70 max-w-[80%] min-w-[80vw] rounded-lg rounded-b-none">
+        <h1 className="text-3xl text-center w-full">
+          {loading ? (
+            <div className="flex items-center justify-center">
+              <Loader className="w-12" />
+              <p>Visualizing</p>
+            </div>
+          ) : (
+            "Visualize"
+          )}
+        </h1>
+
+        <button
+          onClick={copyImageToClipboard}
+          className="border-2 p-2 rounded-lg border-gray-500"
+        >
+          <FaCopy opacity={80} />
+        </button>
+      </div>
+
+      <div className="p-4 bg-gray-300 max-w-[80%] min-w-[80vw] rounded-lg rounded-t-none flex flex-col items-center justify-center">
         {imgUrl ? (
-          <div>
+          <div className="flex flex-col items-center">
             <img src={imgUrl} alt="Generated image" />
           </div>
         ) : (
-          "Submit a document to get started"
+          "Upload a document or write something to start generating image"
         )}
       </div>
     </section>
